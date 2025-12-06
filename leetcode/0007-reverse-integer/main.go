0f703a92ad55478f0f716de59efbdb949af3a6d7
SStoyanov22 <SStoyanov22@gmail.com>
2025-12-06 10:01:26 +0200

    Adding 0007

---
+++ leetcode/0007-reverse-integer/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "math"
+
+func reverse(x int) int {
+	result := 0
+	for x != 0 {
+		// Get the last figit
+		digit := x % 10
+		x /= 10
+		// Max int32: 2147483647, Min int32: -2148483648
+		if result > math.MaxInt32/10 || (result == math.MaxInt32/10 && digit > 7) {
+			return 0
+		}
+
+		if result < math.MinInt32/10 || (result == math.MinInt32/10 && digit < -8) {
+			return 0
+		}
+
+		// Build reversed number
+		result = result*10 + digit
+	}
+
+	return result
+}
